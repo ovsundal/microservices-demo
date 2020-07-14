4587afd36aa6ad5438f9b3ed6767bebbcdcb6b24
osund <osundal83@gmail.com>
2020-07-14 23:03:55 +0200

    add comment component

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -16,8 +18,11 @@ export default () => {
 
     const renderedPosts = Object.values(posts).map(post => {
         return <div className={'card'} style={{width: '30%', marginBottom: '20px'}} key={post.id}>
+
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentList postId={post.id} />
+                <CommentCreate postId={post.id} />
             </div>
         </div>
     });
